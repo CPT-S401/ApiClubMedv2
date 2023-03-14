713cb73b8abd6bf6b13bb01258112dc00d54f818
Mathéo Da Silva <matheo.dasilva@etu.univ-smb.fr>
2023-03-14 08:46:17 +0100

    Ajout Client + User + PasswordhASH (JWT)

---
+++ ApiClubMedv2/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using ApiClubMedv2.Models.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiClubMedv2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetUserData")]
+        [Authorize(Policy = Policies.User)]
+        public IActionResult GetUserData()
+        {
+            return Ok("This is a response from user method");
+        }
+        [HttpGet]
+        [Route("GetAdminData")]
+        [Authorize(Policy = Policies.Admin)]
+        public IActionResult GetAdminData()
+        {
+            return Ok("This is a response from Admin method");
+        }
+    }
+}

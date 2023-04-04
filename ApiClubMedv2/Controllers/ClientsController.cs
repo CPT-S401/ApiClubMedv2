d7feca2335f8c43d72a6713e3a8b239fcb0d9cc5
Mathéo Da Silva <matheo.dasilva@etu.univ-smb.fr>
2023-04-04 17:48:39 +0200

    Ajout ClientsController

---
+++ ApiClubMedv2/Controllers/ClientsController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiClubMedv2.Models.EntityFramework;
+using ApiClubMedv2.Models.Repository;
+
+namespace ApiClubMedv2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly IDataRepositoryUser<Client> _dataRepository;
+
+        public ClientsController(IDataRepositoryUser<Client> dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        // GET : api/Clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return _dataRepository.GetAll();
+        }
+
+        // GET : api/Clients/1
+        [HttpGet]
+        [Route("[action]/{id}")]
+        [ActionName("GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Client>> GetClientById(int id)
+        {
+            var client = _dataRepository.GetById(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return client;
+        }
+
+        // GET : api/Clients/la_plagne
+        [HttpGet]
+        [Route("[action]/{name}")]
+        [ActionName("GetByName")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Client>> GetClientByName(string name)
+        {
+            var client = _dataRepository.GetByString(name);
+            //var utilisateur = await _context.Utilisateurs.FirstOrDefaultAsync(e => e.Mail.ToUpper() == email.ToUpper());
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return client;
+        }
+
+        // PUT: api/Clients/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PutClient(int id, Client client)
+        {
+            if (id != client.IdClient)
+            {
+                return BadRequest();
+            }
+            var clientToUpdate = _dataRepository.GetById(id);
+            if (clientToUpdate == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _dataRepository.Update(clientToUpdate.Value, client);
+                return NoContent();
+            }
+        }
+
+        // POST: api/Clients
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _dataRepository.Add(client);
+            return CreatedAtAction("GetById", new { id = client.IdClient }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            var client = _dataRepository.GetById(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            _dataRepository.Delete(client.Value);
+            return NoContent();
+        }
+    }
+}
---
+++ ApiClubMedv2/Migrations/20230404150738_DeleteJointureCorrectionBd.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiClubMedv2.Migrations
+{
+    public partial class DeleteJointureCorrectionBd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "t_j_clubtypechambre_cth",
+                schema: "clubmed");
+
+            migrationBuilder.RenameColumn(
+                name: "atm_idtypecaracteristique",
+                schema: "clubmed",
+                table: "t_j_activitemultimedia_atm",
+                newName: "atm_idactivite");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "atm_idactivite",
+                schema: "clubmed",
+                table: "t_j_activitemultimedia_atm",
+                newName: "atm_idtypecaracteristique");
+
+            migrationBuilder.CreateTable(
+                name: "t_j_clubtypechambre_cth",
+                schema: "clubmed",
+                columns: table => new
+                {
+                    cth_idclub = table.Column<int>(type: "integer", nullable: false),
+                    cth_idtypechambre = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_j_clubtypechambre_cth", x => new { x.cth_idclub, x.cth_idtypechambre });
+                    table.ForeignKey(
+                        name: "fk_clb_cth",
+                        column: x => x.cth_idclub,
+                        principalSchema: "clubmed",
+                        principalTable: "t_e_club_clb",
+                        principalColumn: "clb_id");
+                    table.ForeignKey(
+                        name: "fk_tch_cth",
+                        column: x => x.cth_idtypechambre,
+                        principalSchema: "clubmed",
+                        principalTable: "t_e_typechambre_tch",
+                        principalColumn: "tch_id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_t_j_clubtypechambre_cth_cth_idtypechambre",
+                schema: "clubmed",
+                table: "t_j_clubtypechambre_cth",
+                column: "cth_idtypechambre");
+        }
+    }
+}
